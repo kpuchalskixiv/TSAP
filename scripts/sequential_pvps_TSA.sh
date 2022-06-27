7c146d55eb48ff46919dae692a00e3b1a08fa0ba
Kacper Puchalski <k.puchalski.xiv@gmail.com>
2022-06-27 22:55:47 +0200

    set sh file AGAIN

--- scripts/sequential_pvps_TSA.sh
+++ scripts/sequential_pvps_TSA.sh
@@ -7,22 +7,19 @@ python3 cli.py \
   --task_name tsa \
   --output_dir experiments/[TASK_NAME]/gpt2/supervised \
   --do_train \
-  --do_eval \
   --do_test \
-  --pet_per_gpu_eval_batch_size 4 \
-  --pet_per_gpu_train_batch_size 4 \
-  --pet_gradient_accumulation_steps 4 \
+  --pet_per_gpu_eval_batch_size 8 \
+  --pet_per_gpu_train_batch_size 8 \
+  --pet_gradient_accumulation_steps 8 \
   --pet_num_train_epochs 10 \
-  --pet_min_steps 1 \
-  --pet_max_steps 2000 \
+
   --pet_max_seq_length 256 \
   --pet_repetitions 1 \
-  --sc_per_gpu_train_batch_size 4 \
-  --sc_per_gpu_unlabeled_batch_size 4 \
-  --sc_gradient_accumulation_steps 4 \
+  --sc_per_gpu_train_batch_size 8 \
+  --sc_per_gpu_unlabeled_batch_size 8 \
+  --sc_gradient_accumulation_steps 8 \
   --sc_num_train_epochs 10 \
-  --sc_min_steps 1 \
-  --sc_max_steps 2000 \
+
   --sc_max_seq_length 256 \
   --sc_repetitions 1 \
   --train_examples 100 500 1000 5000 10000\
